8a3fbd36b9a9cd20bb55526c6c51b3aa5bc26ad0
Alija Ghimire <alijaghimire27@gmail.com>
2024-06-04 14:31:28 +0545

    feat(post):add CRUD API for posts and create relationship with users

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use App\Http\Resources\PostResource;
+use App\Http\Requests\PostRequest;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $posts = Post::all();
+        return response()->json(PostResource::collection($posts));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(PostRequest $request)
+    {
+        $data = $request->validated();
+        if ($request->file('image')) {
+            $request->validate(["image" => ['image', 'mimes:png,jpeg,gif']]);
+            $ext = $request->file('image')->extension();
+            $name = Str::random(20);
+            $path = $name . "." . $ext;
+            $request->file('image')->storeAs('public/images', $path);
+            $data['image'] = "images/" . $path;
+        }
+        $post = Post::create($data);
+        return response()->json(["message" => "Post created successfully", "post" => new PostResource($post)]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PostResource $post)
+    {
+        return response()->json($post);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Post $post)
+    {
+
+        $data = $request->validate([
+            "title" => "required|string",
+            "slug" => "required|string|unique:posts,slug," . $post->id,
+            "description" => "required|string",
+            "status" => "required|in:draft,published",
+            "image" => "nullable|image|mimes:jpeg,png,jpg,gif,svg",
+            "user_id" => "required|exists:users,id",
+        ]);
+
+        if ($request->hasFile('image')) {
+            if ($request->file('image')) {
+
+                $request->validate(["image" => ['image', 'mimes:png,jpeg,gif']]);
+                $ext = $request->file('image')->extension();
+                $name = Str::random(20);
+                $path = $name . "." . $ext;
+                $request->file('image')->storeAs('public/images', $path);
+                $data['image'] = "images/" . $path;
+            }
+
+            if ($post->image) {
+                Storage::delete('public/' . $post->image);
+            }
+        }
+
+        $post->update($data);
+        return response()->json(["message" => "Post updated successfully", "post" => new PostResource($post)]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post $post)
+    {
+        $post->delete();
+        return response()->json(["message" => "Post deleted successfully"]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,15 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post("/register" , [AuthController::class , "register"]);
-Route::post("/login",[AuthController::class, "login"]);
+Route::post("/register" , [AuthController::class, "register"]);
+Route::post("/login", [AuthController::class, "login"]);
 
-Route::resource('post', App\Http\Controllers\PostController::class);
+Route::resource("post", PostController::class);
