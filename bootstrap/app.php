414af5f81944ba3ab4e30cac443abf3f392a4720
Alija Ghimire <alijaghimire27@gmail.com>
2024-06-07 15:20:54 +0545

    feat :middleware registered

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Middleware\CheckIfUserIsBlocked;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Exceptions\ThrottleRequestsException;
+use Illuminate\Routing\Middleware\ThrottleRequests;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,8 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // $middleware->append(CheckIfUserIsBlocked::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        // 
     })->create();
