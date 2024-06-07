82293a6d11234b69842bed0c47daeb653278e6c1
Alija Ghimire <alijaghimire27@gmail.com>
2024-06-07 15:06:58 +0545

    feat : check email from request

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -20,6 +20,12 @@ class AuthController extends Controller
             "password" => "required|min:8|max:20",
         ]);
 
+        if ($request->email && User::where('email', $request->email)->exists()->where('is_blocked', true)) {
+            throw ValidationException::withMessages([
+                "email" => "The provided email is blocked deu to three failed login attempts.",
+            ]);
+        }
+
         $user = User::create($user);
 
         return response()->json(["message" => "User registered successfully", "user" => new AuthResource($user)]);
@@ -35,6 +41,17 @@ class AuthController extends Controller
         $user = User::where("email", $request->email)->first();
 
         if (!$user || !Hash::check($request->password, $user->password)) {
+
+            if ($user) {
+                $this->incrementLoginAttempts($user);
+
+                if ($user->failed_attempts >= 3 || $user->is_blocked) {
+                    $this->lockUserAccount($user);
+                    throw ValidationException::withMessages([
+                        'email' => ['Your account is locked due to three failed login attempts.'],
+                    ]);
+                }
+            }
             throw ValidationException::withMessages([
                 "email" => "The provided email is not correct",
                 "password" => "The provided password is not correct",
@@ -46,9 +63,22 @@ class AuthController extends Controller
             "user" => new AuthResource($user)
         ]);
     }
-    
-    public function logout(Request $request){
+
+    public function logout(Request $request)
+    {
         $request->user()->tokens()->delete();
-        return response()->json(["message"=>"Logged out successfully"]);
+        return response()->json(["message" => "Logged out successfully"]);
+    }
+
+
+    protected function incrementLoginAttempts($user)
+    {
+        $user->increment('failed_attempts');
+    }
+    protected function lockUserAccount($user)
+    {
+        if ($user) {
+            $user->update(['is_blocked' => true]);
+        }
     }
 }
